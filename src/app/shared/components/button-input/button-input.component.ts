b4c3ab903d9227d59f90bc9fc9411bb93f6b4933
Manuel <manueldelpinolucena@gmail.com>
2018-09-17 12:32:34 +0200

    deploy button

--- src/app/shared/components/button-input/button-input.component.ts
+++ src/app/shared/components/button-input/button-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-input',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-input.component.scss']
 })
 export class ButtonInputComponent implements OnInit {
-
+  @Input() text: string;
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
